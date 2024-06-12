e12ce3b1136a07cf5ad50abfadb6922c2d0c01b4
Pgonzales <83090342+Patrick119@users.noreply.github.com>
2024-06-12 16:53:02 -0500

    Se agregó la carpeta de UberService
    Se agregó la carpeta en donde estan todos los modelos, conexiones y configuraciones.

---
+++ uberservice/servicios/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Profesion, Profesional, Servicio, ImagenServicio, SolicitudServicio, Oferta, OrdenServicio, Reseña, Pago, Direccion, DisponibilidadProfesional, MetodoPagoUsuario, ResumenReseñasProfesional, AjusteTarifa, Puntos, Recompensa, Desafio, ProfesionalCercano
+
+admin.site.register(Profesion)
+admin.site.register(Profesional)
+admin.site.register(Servicio)
+admin.site.register(ImagenServicio)
+admin.site.register(SolicitudServicio)
+admin.site.register(Oferta)
+admin.site.register(OrdenServicio)
+admin.site.register(Reseña)
+admin.site.register(Pago)
+admin.site.register(Direccion)
+admin.site.register(DisponibilidadProfesional)
+admin.site.register(MetodoPagoUsuario)
+admin.site.register(ResumenReseñasProfesional)
+admin.site.register(AjusteTarifa)
+admin.site.register(Puntos)
+admin.site.register(Recompensa)
+admin.site.register(Desafio)
+admin.site.register(ProfesionalCercano)
+
