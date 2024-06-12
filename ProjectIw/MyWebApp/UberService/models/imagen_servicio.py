1c8049d6e8ddf4ba74cd5b620fa41b3a725b7035
Pgonzales <83090342+Patrick119@users.noreply.github.com>
2024-06-12 16:51:52 -0500

    Delete ProjectIw directory

--- ProjectIw/MyWebApp/UberService/models/imagen_servicio.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from .servicio import Servicio
-
-class ImagenServicio(models.Model):
-    servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
-    url_imagen = models.CharField(max_length=200)
-    status = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
--- ProjectIw/MyWebApp/UberService/models/profesion.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Profesion(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField()
-    status = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
